8a3b0476fc1263d787c28dcb78b5b862b94d40e2
Kirill Shmakov <shmakir@yandex.ru>
2025-07-28 22:11:55 +0200

    WIP: Separating contextes properly 2.

--- src/vs/workbench/api/common/extHostExtensionService.ts
+++ src/vs/workbench/api/common/extHostExtensionService.ts
@@ -484,17 +484,19 @@ export abstract class AbstractExtHostExtensionService extends Disposable impleme
 
 		const extensionInternalStore = new DisposableStore(); // disposables that follow the extension lifecycle
 		const activationTimesBuilder = new ExtensionActivationTimesBuilder(reason.startup);
+		let epp = '';
 		const whiteList = ['slow-extension', 'norm-extension', 'vscode-mojo', 'c-cpp-compile-run', 'LiveServer', 'prettier-vscode'];
 		if (whiteList.some(item => extensionDescription.identifier.value.includes(item))) {
-			const entryPointPath = joinPath(extensionDescription.extensionLocation, entryPoint).fsPath;
+			epp = joinPath(extensionDescription.extensionLocation, entryPoint).fsPath;
 			const id = extensionDescription.identifier.value.replace(/\./g, '');
 			setAPI('h:id', id);
-			setAPI(`h:entry-point.${id}`, entryPointPath);
+			setAPI(`h:entry-point.${id}`, epp);
+			console.warn(`>>> +id=${id}`);
 			entryPoint = './worker.js';
 		}
 		return Promise.all([
 			this._loadCommonJSModule<IExtensionModule>(extensionDescription, joinPath(extensionDescription.extensionLocation, entryPoint), activationTimesBuilder),
-			this._loadExtensionContext(extensionDescription, extensionInternalStore)
+			this._loadExtensionContext(extensionDescription, extensionInternalStore, epp)
 		]).then(values => {
 			performance.mark(`code/extHost/willActivateExtension/${extensionDescription.identifier.value}`);
 			return AbstractExtHostExtensionService._callActivate(this._logService, extensionDescription.identifier, values[0], values[1], extensionInternalStore, activationTimesBuilder);
@@ -504,7 +506,7 @@ export abstract class AbstractExtHostExtensionService extends Disposable impleme
 		});
 	}
 
-	private _loadExtensionContext(extensionDescription: IExtensionDescription, extensionInternalStore: DisposableStore): Promise<vscode.ExtensionContext> {
+	private _loadExtensionContext(extensionDescription: IExtensionDescription, extensionInternalStore: DisposableStore, epp: string): Promise<vscode.ExtensionContext> {
 
 		const languageModelAccessInformation = this._extHostLanguageModels.createLanguageModelAccessInformation(extensionDescription);
 		const globalState = extensionInternalStore.add(new ExtensionGlobalMemento(extensionDescription, this._storage));
@@ -575,8 +577,12 @@ export abstract class AbstractExtHostExtensionService extends Disposable impleme
 				}
 			});
 
-			const id = extensionDescription.identifier.value.replace(/\./g, '');
-			setAPI(`h:context.${id}`, result);
+			if (epp !== '') {
+				setAPI(epp, result);
+				console.warn(`>>> set=${epp}`);
+			} else {
+				console.warn(`>>> notset`);
+			}
 
 			return result;
 		});
